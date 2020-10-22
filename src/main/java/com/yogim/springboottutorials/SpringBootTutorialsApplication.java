5b039ae80f86cefc0dd676db534f26c6ff190c6e
Yogesh Mhase <yogeshmhase89@gmail.com>
2020-10-21 21:04:19 -0700

    First commit

---
+++ src/main/java/com/yogim/springboottutorials/SpringBootTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.yogim.springboottutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialsApplication.class, args);
+	}
+
+}
